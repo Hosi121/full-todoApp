0f9b1312dca97b448da8677ea48e9d519a67eeed
Hosi121 <takuyakataiwa@icloud.com>
2024-06-24 04:01:43 +0900

    feat:実装

--- todo-app/frontend/src/App.tsx
+++ todo-app/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
 import CreateTask from './components/CreateTask';
+import TaskList from './components/TaskList';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/create-task" element={<CreateTask />} />
+        <Route path="/tasks" element={<TaskList />} />
       </Routes>
     </Router>
   );
